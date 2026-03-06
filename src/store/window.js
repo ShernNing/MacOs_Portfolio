87047794d058ebba9e958f6d9e6dcef3fef3d360
Shern Ning <sherningtan@gmail.com>
2026-03-06 21:07:33 +0800

    Add Zustand store for window management and enhance Dock component functionality

---
+++ src/store/window.js
@@ -0,0 +1,44 @@
+import { INITIAL_Z_INDEX, WINDOW_CONFIG } from "#constants";
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
+
+const useWindowStore = create(
+  immer((set) => ({
+    windows: WINDOW_CONFIG,
+    nextZIndex: INITIAL_Z_INDEX + 1,
+
+    openWindow: (windowKey, data = null) => {
+      set((state) => {
+        const win = state.windows[windowKey];
+        if (win) {
+          win.isOpen = true;
+          win.data = data ?? win.data;
+          win.zIndex = state.nextZIndex;
+          state.nextZIndex++;
+        }
+      });
+    },
+
+    closeWindow: (windowKey) => {
+      set((state) => {
+        const win = state.windows[windowKey];
+        if (win) {
+          win.isOpen = false;
+          win.data = null;
+          win.zIndex = INITIAL_Z_INDEX;
+        }
+      });
+    },
+
+    focusWindow: (windowKey) => {
+      set((state) => {
+        const win = state.windows[windowKey];
+        if (win) {
+          win.zIndex = state.nextZIndex++;
+        }
+      });
+    },
+  })),
+);
+
+export default useWindowStore;
